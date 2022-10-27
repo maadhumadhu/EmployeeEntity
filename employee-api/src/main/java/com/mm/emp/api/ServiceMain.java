fec5f84e27c90d1ab1026f1ef0fc6bb6a5409339
maadhu2711 <madhujamaahu2711@gmail.com>
2022-10-27 17:39:07 +0530

    changes added from feature branch

--- employee-api/src/main/java/com/mm/emp/api/ServiceMain.java
+++ employee-api/src/main/java/com/mm/emp/api/ServiceMain.java
@@ -9,5 +9,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class ServiceMain {
     public static void main(String[] args) {
         SpringApplication.run(ServiceMain.class,args);
+		System.out.println("APP STARTED");
     }
 }
